75ca3a663dbccbcdcc6cffc5ac74ddf6ad85f811
Anurag Gupta <anuragjigupta17@gmail.com>
2018-05-11 19:13:22 +0530

    comparison logic fixed
    Did correction in comparison logic

--- fetch_movie_ratings.py
+++ fetch_movie_ratings.py
@@ -3,18 +3,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-def search_movie(*args):
+def search_movie(titles):
     '''
     Seach for a movie by its name on IMDb
     '''
     
-    print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*58))
-    print("| {0:^10} | {1:^30} | {2:^10} |".format("Sr. No.", "Title", "Ratings"))
-    print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*58))
+    print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*81))
+    print("| {0:^10} | {1:^30} | {2:^10} | {3:^20} |".format("Sr. No.", "Title", "Ratings", "Availability"))
+    print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*81))
 
-    for title in args:
+    for index in range(len(titles)):
         # Get url
-        url = get_url(title)
+        url = get_url(titles[index])
         
         # Get the data
         response = simple_get(url)
@@ -29,15 +29,21 @@ def search_movie(*args):
             # Loop through each title to find the exact match & so its rating
             match = False
             for title_index in range(len(searched_movie_titles)):
-                if is_movie_found(title, searched_movie_titles, title_index):
-                    print("| {0:^10} | {1:<30} | {2:<10} |".format(title, searched_movie_ratings[title_index].get('data-value')))
+                if is_movie_found(titles[index], searched_movie_titles, title_index):
+                    print("| {0:^10} | {1:<30} | {2:^10} | {3:^20} |".format(index+1, titles[index], searched_movie_ratings[title_index].get('data-value'), "(available)"))
+                    print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*81))
                     match = True
                     break
             # Report if the searched movie is not present on IMDb 
             if not match:
-                log_error("Bad luck! \'%s\' is unavailable, Please try again!" %title)
+                print("| {0:^10} | {1:<30} | {2:^10} | {3:^20} |".format(index+1, titles[index], '---', "(unavailable)"))
+                print("{plus}{hyphens}{plus}".format(plus = '+', hyphens = '-'*81))
 
 def is_movie_found(title, searched_movie_titles, title_index):
+    '''
+    Checks the availability of a movie on the IMDb website.
+    Returns True if movie is available, False otherwise.
+    '''
     return ''.join(list(searched_movie_titles[title_index].children)[3].get_text().lower().strip().split()).startswith(''.join(title.lower().split()))
 
 def get_url(title):
@@ -57,7 +63,7 @@ def simple_get(url):
         else:
             return None
     except RequestException as e:
-        log_error("Error during request to {0} : {1}".format(url, str(e)))
+        log_error_msg("Error during request to {0} : {1}".format(url, str(e)))
 
 def is_response_good(response):
     '''
@@ -68,12 +74,15 @@ def is_response_good(response):
             and content_type is not None
             and content_type.find('html') > -1)
 
-def log_error(error):
+def log_error_msg(error):
     '''
     Simply prints the error message.
     '''
     print(error)
 
 if __name__ == '__main__':
-    search_movie("The Godfather", "Star Trek", "The Lord Of The Rings", "The Wolf Of Wall Street", "The Godfather II", "Padmavati", "Annabelle: Creation")
-    search_movie("The GodFaTher III")
+    movies = input("\nGet movies rating, Enter movie(s) name\n( Movies names must be (,) seperated ): ").split(',')
+    movies = [title.strip() for title in movies]
+    search_movie(movies)
+    #search_movie("The Godfather", "Star Trek", "The Lord Of The Rings", "The Wolf Of Wall Street", "The Godfather II", "Padmavati", "Annabelle: Creation")
+    #search_movie("The GodFaTher III")
